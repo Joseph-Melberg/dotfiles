23d8c4394ccb8884f948245829c765403d8582d2
Joseph Benjamin Melberg <jbmelberg@pop-os.localdomain>
2022-06-27 20:19:56 -0500

    More configuration

--- Dependency.sh
+++ Dependency.sh
@@ -6,3 +6,15 @@ sudo apt-get install -y kitty
 sudo apt-get install -y rofi compton fonts-powerline feh
 
 echo "git clone https://github.com/rjekker/i3-battery-popup"
+
+
+cd
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir ~/.fonts/
+mv PowerlineSymbols.otf ~/.fonts/
+mkdir -p .config/fontconfig/conf.d #if directory doesn't exists
+
+fc-cache -vf ~/.fonts/
+
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
